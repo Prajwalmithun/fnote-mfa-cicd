4b95473699b1cca298013bc5d64336368dd58513
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2024-03-08 11:45:08 -0700

    updated ci file

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from website import create_app
 import os
-from flask_recaptcha import ReCaptcha # Import ReCaptcha object
 
 
 app = create_app()
-
-
-# app = Flask(__name__)
-app.config['RECAPTCHA_SITE_KEY'] = '' # <-- Add your site key
-app.config['RECAPTCHA_SECRET_KEY'] = '' # <-- Add your secret key
-recaptcha = ReCaptcha(app) # Create a ReCaptcha object by passing in 'app' as parameter
 
 
 if __name__ == '__main__':
